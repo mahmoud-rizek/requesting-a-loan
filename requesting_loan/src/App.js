15d163b46e08e24b0cf339ff972c87551a5005fe
Mahmoud Abd El-Razek <105005452+mahmoud-rizek@users.noreply.github.com>
2023-11-27 00:22:21 +0200

    empty react project

---
+++ requesting_loan/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LoanForm from "./LoanForm";
+function App() {
+  return (
+    <div className="App">
+      <LoanForm />
+    </div>
+  );
+}
+
+export default App;
