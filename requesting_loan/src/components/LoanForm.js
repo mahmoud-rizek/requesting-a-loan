8afeb689d17390cabe203b37e32a7eca8a888b4d
Mahmoud Abd El-Razek <105005452+mahmoud-rizek@users.noreply.github.com>
2023-12-03 12:16:06 +0200

    design logict for button

--- requesting_loan/src/components/LoanForm.js
+++ requesting_loan/src/components/LoanForm.js
@@ -1,6 +1,19 @@
+import { flushSync } from "react-dom";
 import "./FormStyles.css";
 import Model from "./Model";
+import { useState } from "react";
+
 export default function LoanForm() {
+  const [loanInputs, setLoanInputs] = useState({
+    name: "",
+    phone: "",
+    age: "",
+    sallryRange: "Less than 500$",
+    isEmployee: false,
+  });
+  const btDisable =
+    loanInputs.name == "" || loanInputs.age == "" || loanInputs.phone == "";
+
   return (
     <div className="flex" style={{ flexDirection: "column" }}>
       <form className="flex" id="loan-form" style={{ flexDirection: "column" }}>
@@ -8,25 +21,62 @@ export default function LoanForm() {
         <hr />
 
         <label>Name: </label>
-        <input />
+        <input
+          value={loanInputs.name}
+          onChange={(event) =>
+            setLoanInputs({ ...loanInputs, name: event.target.value })
+          }
+        />
 
         <label>Phone Number: </label>
-        <input />
+        <input
+          value={loanInputs.phone}
+          onChange={(event) =>
+            setLoanInputs({ ...loanInputs, phone: event.target.value })
+          }
+        />
 
         <label>Age: </label>
-        <input />
+        <input
+          value={loanInputs.age}
+          onChange={(event) =>
+            setLoanInputs({ ...loanInputs, age: event.target.value })
+          }
+        />
 
         <label style={{ marginTop: "30px" }}>Are you an employee? </label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={loanInputs.isEmployee}
+          onChange={(event) =>
+            setLoanInputs({ ...loanInputs, isEmployee: event.target.checked })
+          }
+        />
 
         <label>Sallry: </label>
-        <select>
+        <select
+          value={loanInputs.sallryRange}
+          onChange={(event) =>
+            setLoanInputs({ ...loanInputs, sallryRange: event.target.value })
+          }
+        >
           <option>Less than 500$</option>
-          <option>500$</option>
-          <option>More than 500$</option>
+          <option>Between 500$ to 2000</option>
+          <option>Above 2000$</option>
         </select>
 
-        <button id="sumit-laon-btn">Submit</button>
+        <button
+          className={btDisable ? "disabled" : ""}
+          id="sumit-laon-btn"
+          disabled={btDisable}
+          onClick={(event) => {
+            event.preventDefault();
+            alert("hello");
+            console.log(btDisable);
+          }}
+        >
+          Submit
+        </button>
       </form>
       {/* <Model /> */}
     </div>
